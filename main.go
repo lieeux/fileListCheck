f121975bf6b81cef8d0c8dea3f4abfffb1716750
lieeux <290807355@qq.com>
2023-10-17 16:55:58 +0800

    Add files via upload
    使用go协程同步读取和分类输出文件

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fileListCheck/utils"
 	"fmt"
 	"github.com/spf13/viper"
+	"sync"
 	"time"
 )
 
@@ -32,68 +33,125 @@ func main() {
 		panic(fmt.Errorf("Failed to parse config file: %s", err))
 	}
 
-	// 读取小文件afid列表
-	smallFiles, err := utils.ReadAfidList(config.ReadFileAddress + "/afsIndexAfidList.txt")
-	if err != nil {
-		fmt.Println("无法读取小文件afid列表:", err)
-		return
-	}
+	// 创建等待组
+	var wg sync.WaitGroup
 
-	// 读取大文件afid列表
-	largeFiles, err := utils.ReadAfidList(config.ReadFileAddress + "/afsIndexAfidList.txt")
-	if err != nil {
-		fmt.Println("无法读取大文件afid列表:", err)
-		return
-	}
+	// 使用go协程并发执行读取文件任务
+	wg.Add(3)
 
-	// 读取过期文件afid列表
-	expiredFiles, err := utils.ReadAfidList(config.ReadFileAddress + "/afsIndexAfidList.txt")
-	if err != nil {
-		fmt.Println("无法读取过期文件afid列表:", err)
-		return
-	}
+	var smallFiles []string
+	var largeFiles []string
+	var expiredFiles []string
 
-	// 分类文件
-	smallCorrect, smallIncorrect := utils.ClassifyFiles(smallFiles, utils.IsSmallFile)
-	largeCorrect, largeIncorrect := utils.ClassifyFiles(largeFiles, utils.IsLargeFile)
-	expiredCorrect, expiredIncorrect := utils.ClassifyFiles(expiredFiles, utils.IsExpiredFile)
+	go func() {
+		defer wg.Done()
 
-	// 查询小文件是否为seed文件
-	seedFiles, err := utils.FindSeedFiles(smallCorrect)
-	if err != nil {
-		fmt.Println("查询seed文件时发生错误:", err)
-		return
-	}
+		// 读取小文件afid列表
+		var err error
+		smallFiles, err = utils.ReadAfidList(config.ReadFileAddress + "/rfsDataAfidList.txt")
+		if err != nil {
+			fmt.Println("无法读取小文件afid列表:", err)
+		}
+	}()
 
-	// 输出结果到文件
-	err = utils.WriteAfidList(config.WriteFileAddress+"/rfsData_correct.txt", smallCorrect)
-	if err != nil {
-		fmt.Println("无法写入小文件afid正确列表:", err)
-	}
-	err = utils.WriteAfidList(config.WriteFileAddress+"/rfsData_incorrect.txt", smallIncorrect)
-	if err != nil {
-		fmt.Println("无法写入小文件afid分类异常列表:", err)
-	}
-	err = utils.WriteAfidList(config.WriteFileAddress+"/raw_correct.txt", largeCorrect)
-	if err != nil {
-		fmt.Println("无法写入大文件afid正确列表:", err)
-	}
-	err = utils.WriteAfidList(config.WriteFileAddress+"/raw_incorrect.txt", largeIncorrect)
-	if err != nil {
-		fmt.Println("无法写入大文件afid分类异常列表:", err)
-	}
-	err = utils.WriteAfidList(config.WriteFileAddress+"/expired_correct.txt", expiredCorrect)
-	if err != nil {
-		fmt.Println("无法写入过期文件afid正确列表:", err)
-	}
-	err = utils.WriteAfidList(config.WriteFileAddress+"/expired_incorrect.txt", expiredIncorrect)
-	if err != nil {
-		fmt.Println("无法写入过期文件afid异常列表:", err)
-	}
-	err = utils.WriteAfidList(config.WriteFileAddress+"/seed_files.txt", seedFiles)
-	if err != nil {
-		fmt.Println("无法写入seed文件列表:", err)
-	}
+	go func() {
+		defer wg.Done()
+
+		// 读取大文件afid列表
+		var err error
+		largeFiles, err = utils.ReadAfidList(config.ReadFileAddress + "/rawAfidList.txt")
+		if err != nil {
+			fmt.Println("无法读取大文件afid列表:", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		// 读取过期文件afid列表
+		var err error
+		expiredFiles, err = utils.ReadAfidList(config.ReadFileAddress + "/expiredAfidList.txt")
+		if err != nil {
+			fmt.Println("无法读取过期文件afid列表:", err)
+		}
+	}()
+
+	// 等待所有协程完成读取任务
+	wg.Wait()
+
+	// 创建新的等待组,使用go协程并发处理任务
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+
+		// 分类文件
+		smallCorrect, smallIncorrect := utils.ClassifyFiles(smallFiles, utils.IsSmallFile)
+
+		// 输出结果到文件
+		err := utils.WriteAfidList(config.WriteFileAddress+"/rfsData_correct.txt", smallCorrect)
+		if err != nil {
+			fmt.Println("无法写入小文件afid正确列表:", err)
+		}
+		err = utils.WriteAfidList(config.WriteFileAddress+"/rfsData_incorrect.txt", smallIncorrect)
+		if err != nil {
+			fmt.Println("无法写入小文件afid分类异常列表:", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		// 分类文件
+		largeCorrect, largeIncorrect := utils.ClassifyFiles(largeFiles, utils.IsLargeFile)
+
+		// 输出结果到文件
+		err := utils.WriteAfidList(config.WriteFileAddress+"/raw_correct.txt", largeCorrect)
+		if err != nil {
+			fmt.Println("无法写入大文件afid正确列表:", err)
+		}
+		err = utils.WriteAfidList(config.WriteFileAddress+"/raw_incorrect.txt", largeIncorrect)
+		if err != nil {
+			fmt.Println("无法写入大文件afid分类异常列表:", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		// 分类文件
+		expiredCorrect, expiredIncorrect := utils.ClassifyFiles(expiredFiles, utils.IsExpiredFile)
+
+		// 输出结果到文件
+		err := utils.WriteAfidList(config.WriteFileAddress+"/expired_correct.txt", expiredCorrect)
+		if err != nil {
+			fmt.Println("无法写入过期文件afid正确列表:", err)
+		}
+		err = utils.WriteAfidList(config.WriteFileAddress+"/expired_incorrect.txt", expiredIncorrect)
+		if err != nil {
+			fmt.Println("无法写入过期文件afid异常列表:", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		// 查询小文件是否为seed文件
+		seedFiles, err := utils.FindSeedFiles(smallFiles)
+		if err != nil {
+			fmt.Println("查询seed文件时发生错误:", err)
+			return
+		}
+
+		// 输出结果到文件
+		err = utils.WriteAfidList(config.WriteFileAddress+"/seed_files.txt", seedFiles)
+		if err != nil {
+			fmt.Println("无法写入seed文件列表:", err)
+		}
+	}()
+
+	// 等待所有协程完成
+	wg.Wait()
 
 	endTime := time.Now()                 // 记录程序结束时间
 	elapsedTime := endTime.Sub(startTime) // 计算程序运行时间
